e7fe40cb4ea24eee6abaa3a204c79b381ccd0d88
Wiklund <liden.stefan@gmail.com>
2014-09-16 14:00:35 +0200

    image picker up and running, connections and outlets for report view created

--- snaleboda/snaleboda/Report.h
+++ snaleboda/snaleboda/Report.h
@@ -8,6 +8,14 @@
 
 #import <UIKit/UIKit.h>
 
-@interface Report : UIViewController
+@interface Report : UIViewController <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+
+@property (strong, nonatomic) IBOutlet UITextField *name;
+@property (strong, nonatomic) IBOutlet UITextView *description;
+@property (strong, nonatomic) IBOutlet UIImageView *imageView;
+
+- (IBAction)takePhoto:(UIButton *)sender;
+- (IBAction)selectPhoto:(UIButton *)sender;
+- (IBAction)sendReport:(UIButton *)sender;
 
 @end
--- snaleboda/snaleboda/Report.m
+++ snaleboda/snaleboda/Report.m
@@ -10,4 +10,71 @@
 
 @implementation Report
 
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    if (![UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        
+        [self cameraAlert];
+    }
+}
+
+- (void)cameraAlert{
+    UIAlertView *myAlertView = [[UIAlertView alloc] initWithTitle:@"Warning"
+                                                          message:@"The device has no camera"
+                                                         delegate:nil
+                                                cancelButtonTitle:@"OK"
+                                                otherButtonTitles: nil];
+    
+    [myAlertView show];
+}
+
+
+- (IBAction)takePhoto:(UIButton *)sender {
+    
+    
+    if (![UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        
+        [self cameraAlert];
+        return;
+    }
+    
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.delegate = self;
+    picker.allowsEditing = YES;
+    picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+    
+    [self presentViewController:picker animated:YES completion:NULL];
+    
+}
+
+- (IBAction)selectPhoto:(UIButton *)sender {
+    
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.delegate = self;
+    picker.allowsEditing = YES;
+    picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    
+    [self presentViewController:picker animated:YES completion:NULL];
+
+}
+
+- (IBAction)sendReport:(UIButton *)sender {
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    
+    UIImage *chosenImage = info[UIImagePickerControllerEditedImage];
+    self.imageView.image = chosenImage;
+    
+    [picker dismissViewControllerAnimated:YES completion:NULL];
+    
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    
+    [picker dismissViewControllerAnimated:YES completion:NULL];
+    
+}
+
 @end
